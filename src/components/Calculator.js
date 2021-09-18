86d5cd4c0aa2d78d0a7531924c08107cc258f1bb
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-18 22:08:48 +0500

    Replace class based components states with react hooks ( useState )

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 import Button from './Button';
 import Output from './Output';
 
@@ -27,29 +28,40 @@ const Calculator = () => {
     operation: null,
   });
 
+  const clickEvent = (event) => {
+    const obj = calcState;
+    console.log('State: ', obj);
+    const { value } = event.target;
+    const resultObject = calculate(obj, value);
+    console.log('Result Object: ', resultObject);
+    calcSetState(resultObject);
+  };
+
+  const outputVal = generateResult(calcState);
+  console.log('Output Value: ', outputVal);
   return (
     <section className="calculator-container">
       <main className="grid-container">
-        <Output />
-        <Button className="ac" value="AC" />
-        <Button className="add-sub" value="+/-" />
-        <Button className="modulus" value="%" />
-        <Button className="divide" value="÷" />
-        <Button className="seven" value="7" />
-        <Button className="eight" value="8" />
-        <Button className="nine" value="9" />
-        <Button className="cross" value="x" />
-        <Button className="four" value="4" />
-        <Button className="five" value="5" />
-        <Button className="six" value="6" />
-        <Button className="minus" value="-" />
-        <Button className="one" value="1" />
-        <Button className="two" value="2" />
-        <Button className="three" value="3" />
-        <Button className="add" value="+" />
-        <Button className="zero" value="0" />
-        <Button className="dot" value="." />
-        <Button className="equal" value="=" />
+        <Output outputval={outputVal} clickEvent={clickEvent} />
+        <Button className="ac" value="AC" clickEvent={clickEvent} />
+        <Button className="add-sub" value="+/-" clickEvent={clickEvent} />
+        <Button className="modulus" value="%" clickEvent={clickEvent} />
+        <Button className="divide" value="÷" clickEvent={clickEvent} />
+        <Button className="seven" value="7" clickEvent={clickEvent} />
+        <Button className="eight" value="8" clickEvent={clickEvent} />
+        <Button className="nine" value="9" clickEvent={clickEvent} />
+        <Button className="cross" value="x" clickEvent={clickEvent} />
+        <Button className="four" value="4" clickEvent={clickEvent} />
+        <Button className="five" value="5" clickEvent={clickEvent} />
+        <Button className="six" value="6" clickEvent={clickEvent} />
+        <Button className="minus" value="-" clickEvent={clickEvent} />
+        <Button className="one" value="1" clickEvent={clickEvent} />
+        <Button className="two" value="2" clickEvent={clickEvent} />
+        <Button className="three" value="3" clickEvent={clickEvent} />
+        <Button className="add" value="+" clickEvent={clickEvent} />
+        <Button className="zero" value="0" clickEvent={clickEvent} />
+        <Button className="dot" value="." clickEvent={clickEvent} />
+        <Button className="equal" value="=" clickEvent={clickEvent} />
       </main>
     </section>
   );
