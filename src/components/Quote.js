2a11512f99f377dd370f7b392702ac9e3633549e
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-22 03:37:23 +0500

    Create quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quote = () => {
+  return (
+    <section>
+      <p>
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: It is about understanding. -- William Paul Thurston
+      </p>
+    </section>
+  );
+};
+
+export default Quote;
