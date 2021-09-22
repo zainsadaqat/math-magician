8a97968579e698c92991b16ecb30167c9a6f0e7f
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-22 19:24:13 +0500

    Develop navbar using flexbox

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -8,13 +9,13 @@ const Navbar = () => {
       <nav>
         <ul className="nav">
           <li className="nav-item">
-            <a href="#home">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <a href="#calculator">Calculator</a>
+            <Link to="/calculator">Calculator</Link>
           </li>
           <li className="nav-item">
-            <a href="#quote">Quote</a>
+            <Link to="/quote">Quote</Link>
           </li>
         </ul>
       </nav>
