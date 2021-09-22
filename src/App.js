ce7ef0c168d4f1dd0ac32f3111d9170198436be4
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-22 19:24:58 +0500

    Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import React from 'react';
-import Calculator from './components/Calculator';
 import './App.css';
+import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Quote from './components/Quote';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App container">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/calculator">
+            <Calculator />
+          </Route>
+          <Route exact path="/quote">
+            <Quote />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
