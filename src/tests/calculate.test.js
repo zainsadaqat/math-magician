9fd56ff72c4293273acd4be34fe503303e5ea418
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-24 03:07:06 +0500

    Write tests for minus operator in calculate.js file

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -2,7 +2,7 @@ import calculate from '../logic/calculate';
 import '@testing-library/jest-dom';
 
 describe('Test calculate.js', () => {
-  test('Test divide method in calculate', () => {
+  test('Test divide function in calculate', () => {
     let obj = {
       total: null,
       next: null,
@@ -14,7 +14,7 @@ describe('Test calculate.js', () => {
     obj = calculate(obj, '=');
     expect(obj.total).toEqual('4');
   });
-  test('Test multiplication method in calculate', () => {
+  test('Test multiplication function in calculate', () => {
     let obj = {
       total: null,
       next: null,
@@ -26,7 +26,7 @@ describe('Test calculate.js', () => {
     obj = calculate(obj, '=');
     expect(obj.total).toEqual('16');
   });
-  test('Test plus method in calculate', () => {
+  test('Test plus function in calculate', () => {
     let obj = {
       total: null,
       next: null,
@@ -37,5 +37,17 @@ describe('Test calculate.js', () => {
     obj = calculate(obj, '2');
     obj = calculate(obj, '=');
     expect(obj.total).toEqual('10');
+  });
+  test('Test minus function in calculate', () => {
+    let obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    obj = calculate(obj, '8');
+    obj = calculate(obj, '-');
+    obj = calculate(obj, '2');
+    obj = calculate(obj, '=');
+    expect(obj.total).toEqual('6');
   });
 });
