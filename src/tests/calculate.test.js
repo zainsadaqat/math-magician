f01608d2edab0f549f7b8da216df3a83197b0d1a
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-23 22:15:12 +0200

    Add tests for caculate function

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,29 @@
+import calculate from '../logic/calculate';
+import '@testing-library/jest-dom';
+
+describe('Test calculate.js', () => {
+  test('Test divide method in calculate', () => {
+    let obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    obj = calculate(obj, '8');
+    obj = calculate(obj, '÷');
+    obj = calculate(obj, '2');
+    obj = calculate(obj, '=');
+    expect(obj.total).toEqual('4');
+  });
+  test('Test multiplication method in calculate', () => {
+    let obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    obj = calculate(obj, '8');
+    obj = calculate(obj, 'x');
+    obj = calculate(obj, '2');
+    obj = calculate(obj, '=');
+    expect(obj.total).toEqual('16');
+  });
+});
